fb2577e59630e5c84420c03c9f778d71760aae1b
Shiva Sharma <shivasharmajpr2003@gmail.com>
2023-01-25 17:55:24 +0530

    Get Profile Implemented

--- server/Controller/GetController.js
+++ server/Controller/GetController.js
@@ -32,15 +32,42 @@ const GetTimelineController = asyncHandler(async (req, res) => {
                     }
                 }
             ])
-            res.status(200).json(CurrentUserPosts.concat({...FollowingPosts[0].followingPosts}).sort((a, b) => {
+            res.status(200).json(CurrentUserPosts.concat({ ...FollowingPosts[0].followingPosts }).sort((a, b) => {
                 return b.createdAt - a.createdAt
             }))
         } else {
             res.status(404).json({ Message: "User Not Found" })
         }
     } catch (error) {
-
+        res.status(500).json({ errorMessage: error.message });
     }
 })
 
-export { GetTimelineController }
+const GetProfileController = asyncHandler(async (req, res) => {
+    try {
+        const user = await UserModal.findById(req.user._id).select("-Password")
+        if (user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({ errorMessage: "User Not Found" })
+        }
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+})
+
+const GetProfileByIdController = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    try {
+        const user = await UserModal.findById(id).select("-password").select("-email -phoneNumber -Password")
+        if (user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({ errorMessage: "User Not Found" })
+        }
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+})
+
+export { GetTimelineController,GetProfileByIdController,GetProfileController }
--- server/Routes/GetRoute.js
+++ server/Routes/GetRoute.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import Protect from '../Middleware/Auth/ProtectRoute.js'
-import { GetTimelineController } from '../Controller/GetController.js'
+import { GetTimelineController,GetProfileByIdController,GetProfileController } from '../Controller/GetController.js'
 const router = express.Router()
 
 router.route("/timeline").get(Protect, GetTimelineController)
+router.route("/profile").get(Protect,GetProfileController)
+router.route("/profile/:id").get(GetProfileByIdController)
+
 
 export default router;
